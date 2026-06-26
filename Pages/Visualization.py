cd90c7616c2eb2656fd0d158921e30e3966827d1
Tjeeiv <siddharthvelusamy@gmail.com>
2026-06-26 15:55:57 +0530

    visualization completed

--- Pages/Visualization.py
+++ Pages/Visualization.py
@@ -1,6 +1,5 @@
 import streamlit as st 
-from Services.visualizeservice import fetchgolddata, plottopcategories
-
+from Services.visualizeservice import fetchgolddata, plottopcategories,plotmonthlytrend, plotreviewdistribution
 st.title("Data visualization")
 
 st.title("Visual Analytics")
@@ -16,6 +15,13 @@ if st.button("Generate Visualization"):
             fig1 = plottopcategories(sales)
             st.pyplot(fig1)
 
+            st.header("2. Monthly Sales Trend")
+            fig2 = plotmonthlytrend(sales)
+            st.pyplot(fig2)
+
+            st.header("3. Review Distribution")
+            fig3 = plotreviewdistribution(reviews)
+            st.pyplot(fig3)
 
         except Exception as e:
             st.error(f"Error:{e}")
--- Services/visualizeservice.py
+++ Services/visualizeservice.py
@@ -26,8 +26,32 @@ def plottopcategories(sales_df, top_n=10):
 
     fig, ax = plt.subplots(figsize=(10, 6))
     sns.barplot(x=top.values, y=top.index, ax=ax, palette="viridis")
+    ax.ticklabel_format(style='plain', axis='x')
     ax.set_xlabel("Total Revenue")
     ax.set_ylabel("Product Category")
     ax.set_title(f"Top {top_n} Performing Product Categories by Revenue")
     plt.tight_layout()
+    return fig
+
+
+
+def plotmonthlytrend(sales_df):
+    data = sales_df.groupby("ORDERMONTH")["TOTALREVENUE"].sum().sort_index()
+
+    fig, ax = plt.subplots(figsize=(10,5))
+    ax.plot(data.index ,data.values, marker="o")
+    ax.set_xlabel("Month")
+    ax.set_ylabel("Total Revenue")
+    ax.set_title("Monthly Revenue Trend"    )
+    plt.xticks(rotation = 45)
+    plt.tight_layout()
+    return fig
+
+def plotreviewdistribution(reviewdf):
+
+    fig,ax = plt.subplots(figsize = (10,5))
+    sns.countplot(data=reviewdf , x="REVIEWSCORE",ax=ax)
+    ax.set_xlabel("Review")
+    ax.set_ylabel("Total count")
+    ax.set_title("Review Distribution"    )     
     return fig
